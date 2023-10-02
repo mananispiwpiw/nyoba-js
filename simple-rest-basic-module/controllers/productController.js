b36aa1b303701a2b842ba4c2276fad002acf7031
mananispiwpiw <rafijaeger@gmail.com>
2023-10-02 13:34:45 +0700

    add getProduct() to controller

--- simple-rest-basic-module/controllers/productController.js
+++ simple-rest-basic-module/controllers/productController.js
@@ -1,5 +1,7 @@
 import Product from '../models/productModel.js';
 
+// @desc    Gets all products/items
+// @route   GET /api/products
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll();
@@ -7,6 +9,26 @@ async function getProducts(req, res) {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify(products));
         res.end();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// @desc    Gets single product/item
+// @route   GET /api/product/id
+async function getProduct(req, res) {
+    try {
+        const product = await Product.findByID();
+
+        if (!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({ message: 'Product not found.' }));
+            res.end();
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(product));
+            res.end();
+        }
     } catch (error) {
         console.log(error);
     }
